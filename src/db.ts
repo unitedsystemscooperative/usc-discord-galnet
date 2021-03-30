b0878280d09d39a781b5b764c91b6d382c0410f9
Zachary Bryant <Zachary@admiralfeb.net>
2021-03-30 14:30:19 -0500

    fix: db connections

--- src/db.ts
+++ src/db.ts
@@ -6,32 +6,50 @@ const connectionString = `mongodb+srv://${username}:${userpass}@cluster0.xup6s.m
 const client = new MongoClient(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  maxIdleTimeMS: 10000,
+  serverSelectionTimeoutMS: 10000,
+  socketTimeoutMS: 20000,
 });
 
 const collectionName = 'latestGalNet';
 
+export const connectToMongo = async () => {
+  if (!client.isConnected()) {
+    try {
+      await client.connect();
+    } catch (err) {
+      console.error('Failed to connect');
+    }
+  }
+  return client;
+};
+
 export const getFromMongo = async (query?: any) => {
   let response: any[] = [];
   try {
-    await client.connect();
+    await connectToMongo();
+    try {
+      const database = client.db('usc');
+      const collection = database.collection(collectionName);
 
-    const database = client.db('usc');
-    const collection = database.collection(collectionName);
-
-    if (query && Object.keys(query).includes('index')) {
-      query.index = parseInt(query.index);
+      if (query && Object.keys(query).includes('index')) {
+        query.index = parseInt(query.index);
+      }
+      const cursor = collection.find(query);
+      response = await cursor.toArray();
+      cursor.close();
+      return response;
+    } catch (err) {
+      console.error(err);
     }
-    const cursor = collection.find(query);
-    response = await cursor.toArray();
-    return response;
   } catch (err) {
-    console.error(err);
+    console.error('Failed to connect');
   }
 };
 
 export const sendToMongo = async (updateDoc: any, query?: any) => {
   try {
-    await client.connect();
+    await connectToMongo();
 
     const database = client.db('usc');
     const collection = database.collection(collectionName);
